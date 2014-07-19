a577bd768e44e40e4cd6f69f9cb2e145f4e80a1d
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-07-19 10:39:09 +0200

    added the smallest set of files needed for the plugin

---
+++ edit_phov.cpp
@@ -0,0 +1,48 @@
+/****************************************************************************
+ * MeshLab                                                           o o     *
+ * A versatile mesh processing toolbox                             o     o   *
+ *                                                                _   O  _   *
+ * Copyright(C) 2005                                                \/)\/    *
+ * Visual Computing Lab                                            /\/|      *
+ * ISTI - Italian National Research Council                           |      *
+ *                                                                    \      *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *   
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ *                                                                           *
+ ****************************************************************************/
+/****************************************************************************
+  History
+$Log: meshedit.cpp,v $
+****************************************************************************/
+
+#include <math.h>
+#include <stdlib.h>
+#include <meshlab/glarea.h>
+#include "edit_phov.h"
+#include <wrap/gl/pick.h>
+#include <wrap/qt/gl_label.h>
+
+using namespace std;
+using namespace vcg;
+
+EditPhovPlugin::EditPhovPlugin() {
+	qFont.setFamily("Helvetica");
+	qFont.setPixelSize(12);    
+}
+
+const QString EditPhovPlugin::Info() 
+{
+	return tr("Return detailed info about a picked face of the model.");
+}
+
+
---
+++ edit_phov.pro
@@ -0,0 +1,10 @@
+include (../../shared.pri)
+
+
+HEADERS       = edit_phov.h
+				 
+SOURCES       = edit_phov.cpp 
+
+TARGET        = edit_phov
+
+RESOURCES     = edit_phov.qrc
